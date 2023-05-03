98e39f707aafd4be831a43415f54f0c48d4db239
rusmansutardi <101523097+mansitee@users.noreply.github.com>
2023-05-03 12:24:48 +0700

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
                     <div className="flex justify-start lg:w-0 lg:flex-1">
-                        <a href="/" className="text-white font-bold text-xl">ReacRadio</a>
+                        <a href="/" className="text-white font-bold text-xl">Radio</a>
                     </div>
                     <div className="flex items-center md:ml-auto">
                         <a href="https://github.com/NeeRaj-2401/ReacRadio" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-300">
